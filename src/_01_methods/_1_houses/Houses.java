7ee2ec51fb354c4bacdcc9fd17c99f0f0bebbc31
league <karinabriellen@gmail.com>
2023-11-01 20:07:14 -0700

    pi

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,5 +1,9 @@
 package _01_methods._1_houses;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /*
@@ -7,16 +11,41 @@ import org.jointheleague.graphical.robot.Robot;
  *    Level 1
  */
 public class Houses {
-		
+	Robot rob = new Robot();
 	public void run() {
-		Robot rob = new Robot();
-		rob.moveTo(600, 300);
+		rob.moveTo(50, 400);
 		rob.penDown();
 		rob.setPenWidth(5);
 		rob.setSpeed(100);
 		rob.turn(90);
-		rob.move(30);
-		rob.turn(90);
-		rob.move(100);
+		drawGrass();
+		for(int i = 0; i< 10; i++) {
+		String heightAsString = JOptionPane.showInputDialog(null,"Pick a height");
+		int height = Integer.parseInt(heightAsString);
+		combo(height);
+		}
+		
+	}
+	public void combo(int height) {
+		buildHouse(height);
+		drawGrass();
+	}
+	 public void buildHouse(int height) {
+		 Random colorRan = new Random();
+		 int color = colorRan.nextInt(255);
+		 int color2 = colorRan.nextInt(255);
+		int color3 = colorRan.nextInt(255);
+		 rob.setPenColor(color, color2, color3);
+		 rob.turn(270);
+			rob.move(height);
+			rob.turn(90);
+			rob.move(40);
+			rob.turn(90);
+			rob.move(height);
+			rob.turn(270);
+	 }
+	public void drawGrass() {
+		rob.setPenColor(40, 130, 40);
+		rob.move(25);
 	}
 }
